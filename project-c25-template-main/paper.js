edd5ee08bdb793d3efdb6e8899640e698940d3b2
ahaangupta <80193060+ahaangupta@users.noreply.github.com>
2021-04-16 16:52:30 +1000

    Add files via upload

---
+++ project-c25-template-main/paper.js
@@ -0,0 +1,33 @@
+class Paper {
+    constructor(x,y,w,h)
+	{
+		var options={
+			isStatic:false,
+            restitution:0.5,
+            friction:0,
+            density:0.1			
+			}
+		this.x=x;
+		this.y=y;
+		this.w=w
+		this.h=h
+        this.image = loadImage("paper.png")
+		this.body=Bodies.rectangle(x, y, w, h , options);
+ 		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var groundPos=this.body.position;		
+
+			push()
+			translate(groundPos.x, groundPos.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			fill(128,128,128)
+            image(this.image,0,0,this.w,this.h)
+			pop()
+			
+	}
+}
